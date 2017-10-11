8ed2f290549c1ab1e59e7dc29c75ec67a8350a94
sobo lana <botezatu.richard@gmail.com>
2017-10-11 07:28:37 +0200

    Archived users cannot sign in to the app

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Users can sign in" do
-  let!(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
 
   scenario "with valid credentials" do
     visit "/"
@@ -12,5 +12,15 @@ RSpec.feature "Users can sign in" do
 
     expect(page).to have_content "Signed in successfully"
     expect(page).to have_content "Signed in as #{user.email}"
+  end
+
+  scenario "unless they are archived" do
+    user.archive
+
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Sign in"
   end
 end
