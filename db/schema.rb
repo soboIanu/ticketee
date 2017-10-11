c6f85b8a44e95c11fb0517f12cb300bca4cfd165
sobo lana <botezatu.richard@gmail.com>
2017-10-11 05:08:19 +0200

    Added admin flag to User model, and seeded the first admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010224548) do
+ActiveRecord::Schema.define(version: 20171011030034) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20171010224548) do
     t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tickets_on_author_id"
     t.index ["project_id"], name: "index_tickets_on_project_id"
   end
 
@@ -41,6 +43,7 @@ ActiveRecord::Schema.define(version: 20171010224548) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
